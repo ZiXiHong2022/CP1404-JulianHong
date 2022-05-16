c2050a9cb3ef01f5af0bd59b503c657f18ead2b1
ZiXiHong2022 <xijulian65@gmail.com>
2022-05-17 00:23:17 +0800

    commit question 3

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -3,21 +3,20 @@ from taxi import Taxi
 class SilverServiceTaxi(Taxi):
     value = 0
 
-    def __init__(self, name, fuel, price_per_km):
+    def __init__(self, name, fuel, fanciness):
         Taxi.__init__(self, name, fuel)
-        self.price_per_km = price_per_km
+        self.fanciness = fanciness
         self.flag_fall = 4.50
 
-    def fanciness(self, value):
-        return float(self.price_per_km) * value
+    def fanciness(self, fanciness):
+        return float(self.price_per_km) * fanciness
 
     def total_fare(self):
-        fare = self.fanciness(2) * self.current_fare_distance
+        fare = self.get_fare()
         return self.flag_fall + fare
 
     def __str__(self):
-        word = super.__str__(self)
-        return word + "plus flag fall $" + str(self.flag_fall)
+        return super().__str__() + "plus flag fall $" + str(self.flag_fall)
 
 
 
