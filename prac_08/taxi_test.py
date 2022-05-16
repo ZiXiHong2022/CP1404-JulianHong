f7957d971b9bbb6dcc7e6a1ddb359511c2ebd461
ZiXiHong2022 <xijulian65@gmail.com>
2022-05-16 15:31:52 +0800

    finish question one

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,13 @@
+from taxi import Taxi
+taxi_detail = Taxi("Prius 1", 100, 1.23)
+taxi_detail.current_fare_distance = 40
+print(taxi_detail)
+print(taxi_detail.get_fare())
+
+
+taxi_detail.start_fare()
+taxi_detail.current_fare_distance = 100
+print(taxi_detail)
+print(taxi_detail.get_fare())
+
+
